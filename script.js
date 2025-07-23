630247cf0d684c4c3f11df724db7ba09f3833974
xavpav9 <192867446+xavpav9@users.noreply.github.com>
2025-07-23 13:42:18 +0100

    Fix operate function to take in string, not func
    Also remove global operand and operator variables.

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-let leftOperand;
-let rightOperand;
-let operator;
-
 function add(num1, num2) {return num1 + num2};
 function subtract(num1, num2) {return num1 - num2};
 function multiply(num1, num2) {return num1 * num2};
 function divide(num1, num2) {return num1 / num2};
 
-function operate(num1, num2, operation) {return operation(num1, num2)};
+function operate(leftOperand, operator, rightOperand) {
+  switch (operator) {
+    case "+":
+      return add(leftOperand, rightOperand);
+      break;
+
+    case "-":
+      return subtract(leftOperand, rightOperand);
+      break;
+
+    case "*":
+      return multiply(leftOperand, rightOperand);
+      break;
+
+    case "/":
+      return divide(leftOperand, rightOperand);
+      break;
+  };
+};
