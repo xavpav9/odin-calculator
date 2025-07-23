f6be5ac503cf1bcd14095664c81dc05e8dce1bd3
xavpav9 <192867446+xavpav9@users.noreply.github.com>
2025-07-23 14:29:41 +0100

    Make number and operator buttons functional
    Can do operations on numbers and get the result. Numbers disappear from
    display after number input after operator input.

--- script.js
+++ script.js
@@ -2,10 +2,33 @@ const calculator = document.querySelector(".calculator");
 const displayText = document.querySelector(".display .text");
 
 const numbers = "0 1 2 3 4 5 6 7 8 9".split(" ");
+const operators = "+ - x ÷".split(" ");
+let operatorReady = false;
+let operatorJustReady = false;
+let leftOperand;
+let operator;
+
 calculator.addEventListener("click", evt => {
   if (evt.target.tagName === "BUTTON") {
     if (numbers.includes(evt.target.textContent)) {
+      if (operatorJustReady) {
+        displayText.textContent = "";
+        operatorJustReady = false;
+      }
       displayText.textContent += evt.target.textContent; 
+    };
+
+    if (operators.includes(evt.target.textContent) && !operatorReady) {
+      leftOperand = +displayText.textContent;
+      operator = evt.target.textContent;
+      operatorReady = true;
+      operatorJustReady = true;
+    } else if ((evt.target.textContent === "=" || operators.includes(evt.target.textContent)) && operatorReady && displayText.textContent !== "") {
+      leftOperand = Math.round(operate(leftOperand, operator, +displayText.textContent) * 100) / 100;
+      displayText.textContent = leftOperand;
+      if (evt.target.textContent === "=") operatorReady = false;
+      else operator = evt.target.textContent;
+      operatorJustReady = true; 
     }
   }
 });
@@ -25,11 +48,11 @@ function operate(leftOperand, operator, rightOperand) {
       return subtract(leftOperand, rightOperand);
       break;
 
-    case "*":
+    case "x":
       return multiply(leftOperand, rightOperand);
       break;
 
-    case "/":
+    case "÷":
       return divide(leftOperand, rightOperand);
       break;
   };
