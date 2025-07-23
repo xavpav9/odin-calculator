f95b6aabbd19b3d7f1f63d12bf44c278da585ead
xavpav9 <192867446+xavpav9@users.noreply.github.com>
2025-07-23 12:37:43 +0100

    Add operator, operand variables and operate func

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+let leftOperand;
+let rightOperand;
+let operator;
+
 function add(num1, num2) {return num1 + num2};
 function subtract(num1, num2) {return num1 - num2};
 function multiply(num1, num2) {return num1 * num2};
 function divide(num1, num2) {return num1 / num2};
+
+function operate(num1, num2, operation) {return operation(num1, num2)};
